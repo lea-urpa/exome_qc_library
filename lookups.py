457f600c66157e07e333c214676c2d27b3fff3d9
lea-urpa <lea.urpa@gmail.com>
2020-08-03 15:00:49 +0300

    added more print statements?

--- lookups.py
+++ lookups.py
@@ -10,9 +10,8 @@ import variant_annotation as va
 # def filter_to_samples(mt, samples):
 
 
-def calculate_carrier_counts_ids(mt, qc_done=True):
+def calculate_carrier_counts_ids(mt):
     """
-    """"""
     Calculate for a matrix table the number of het and hom alt genotypes per variant, and the carriers for each.
 
     :param mt: matrix table to annotate
@@ -28,8 +27,7 @@ def calculate_carrier_counts_ids(mt, qc_done=True):
 
 
 def get_lof_carriers(mt):
-
-    # Annotate a row annotation for the number of hom alt and het carriers if the variant is LOF
+    # Add a row annotation for the number of hom alt and het carriers if the variant is LOF
     mt = mt.annotate_rows(homalt_lof_count=hl.agg.count_where((mt.LOF == True) & (mt.hom_alt_gt_count > 0)))
     mt = mt.annotate_rows(het_lof_count=hl.agg.count_where((mt.LOF == True) & (mt.het_gt_count > 0)))
     # TODO check whether this produces 0 or missing if mt.LOF is false
@@ -40,26 +38,31 @@ def get_lof_carriers(mt):
 
     # If so, filter to variants that are LOF and the counts are greater than zero
     if (lof_homalt_count > 0) or (lof_het_count > 0):
+        print("%s homalt and %s het carriers found." % (lof_homalt_count, lof_het_count))
         lofmt = mt.filter_rows((mt.LOF == True) & ((mt.het_lof_count > 0) | (mt.homalt_lof_count > 0)))
 
         # Then filter to individuals that are hom alt or het for the remaining samples
         lofmt = lofmt.annotate_cols(alt_gt_count=hl.agg.count_where(lofmt.GT.is_hom_var() | lofmt.GT.is_het()))
+        lofmt = lofmt.filter_cols(lofmt.alt_gt_count > 0)
 
         # Then annotate the carriers for each variant
         lofmt = lofmt.annotate_rows(het_carriers=hl.agg.filter(lofmt.GT.is_het(), hl.agg.collect(mt.s)))
-        lofmt = lofmt.annotate_rows(hom_alt_carriers=hl.agg.filter(lofmt.GT.is_hom_var(), hl.agg.collect(mt.s)))
+        lofmt = lofmt.annotate_rows(hom_alt_carriers=hl.agg.filter(lofmt.GT.is_hom_var(), hl.agg.collect(lofmt.s)))
 
         # Then export the rows as a table, print and save to file
         carriers_ht = lofmt.rows()
         carriers_ht.show(-1) # TODO test this
-        print('Saving this table to het_carriers.txt')
-
-        carriers_ht.export("het_carriers.txt")
     else:
         carriers_ht = None
 
-    return lof_homalt_count, lof_het_count, carriers_ht
+    return carriers_ht
 
+
+#def get_missense_carriers(mt):
+    # Test for CADD and MPC annotations
+
+
+    # Add a new row annotation for the number of hom alt and het carriers if the variant is damaging missense
 
 def get_gene_damaging_carriers(mt, gene, report_all_homozygotes=True):
 
@@ -87,6 +90,11 @@ def get_gene_damaging_carriers(mt, gene, report_all_homozygotes=True):
     genemt = calculate_carrier_counts_ids(genemt)
 
     # Report damaging variants + carriers
-    lof_carriers = get_lof_carriers(genemt)
     print('Checking for LOF variants in gene %s' % gene)
+    lof_homalt_count, lof_het_count, carriers_ht = get_lof_carriers(genemt)
+
+    print('Saving this table to het_carriers.txt')
+    carriers_ht.export("%s_het_carriers.txt" % gene)
+
+    print('Checking for damaging missense variants in gene %s' % gene)
 
