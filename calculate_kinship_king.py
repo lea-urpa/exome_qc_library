9017c202c3133fd41691054c60845bfbc39c729c
lea-urpa <lea.urpa@gmail.com>
2020-08-18 16:27:55 +0300

    fixed typo

--- calculate_kinship_king.py
+++ calculate_kinship_king.py
@@ -36,7 +36,7 @@ def run_king(args):
         input_name = args.plink_data + '.bed'
 
     cmd = f"{args.king_path} -b {input_name} --kinship --prefix {args.plink_data} --degree 3"
-    print(f"King commnad to run: {cmd}")
+    print(f"King command to run: {cmd}")
     subprocess.call(cmd.split())
 
 
