28d36c5c021786202c233d1bd010e06ad7418276
lea-urpa <lea.urpa@gmail.com>
2020-09-23 14:36:34 +0300

    initial commit

---
+++ validate_datasets.py
@@ -0,0 +1,6 @@
+"""
+Scripts to validate that the samples in two datasets are the same individuals as expected
+(e.g. comparing exome sequencing and genotyping data)
+"""
+
+
---
+++ validate_pedigree.py
@@ -0,0 +1,17 @@
+"""
+Scripts to take King genetic relatedness information + the pedigree/fam file that went into King, and creates
+a report of the errors and the number of families/sizes of families in the dataset.
+"""
+
+# Read in .kin file and report errors in readable way
+
+
+# Read in .kin0 file and report errors in readable way
+
+
+
+# Create edgelist from kin file without errors and report # of families, and size of families
+
+# Create edgelist from kin file without errors and kin0 file and report # of families, and size of families (subtracting
+# individuals who are related to too many other individuasl > likely mixups)
+
