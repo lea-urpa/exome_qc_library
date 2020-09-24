3a7fa7a0ffabd9beffbbb0d7efbb033ac098e154
lea-urpa <lea.urpa@gmail.com>
2020-09-24 14:48:51 +0300

    changed delim input to automatic check with utility function, wrote function to count families given edgelist

--- validate_pedigree.py
+++ validate_pedigree.py
@@ -10,7 +10,8 @@ a report of the errors and the number of families/sizes of families in the datas
 import time
 import os
 import networkx as nx
-from calculate_kinship_king import create_edgelist
+from collections import OrderedDict
+import utils
 
 
 def infer_relationship(ibs0, kinship):
@@ -36,11 +37,10 @@ def infer_relationship(ibs0, kinship):
     return actual
 
 
-def parse_kin_errors(kin_file, delim, cohorts, args):
+def parse_kin_errors(kin_file, cohorts, args):
     """
     Read in .kin file and report errors in readable way
     :param kin_file: .kin output from King
-    :param delim: delimiter for King output
     :param cohorts: array of strings with cohort subset strings to subset report to.
     :param args: output stem to write to
     :return:
@@ -56,6 +56,7 @@ def parse_kin_errors(kin_file, delim, cohorts, args):
         error_report.write(f"#Input kinship file: {kin_file}\n")
         error_report.write(f"#Date and time: {time.asctime()}\n")
         kinship_errors = 0
+        delim = utils.check_delimiter(kin_file)
         with open(kin_file) as in_kin:
             for line in in_kin:
                 # Create separate report for all errors in kinship file, or just IDs containing cohort of interest
@@ -104,7 +105,7 @@ def parse_kin_errors(kin_file, delim, cohorts, args):
         in_kin.close()
 
 
-def parse_kin0_errors(kin0_file, delim, cohorts, args):
+def parse_kin0_errors(kin0_file, cohorts, args):
     """
     Read in .kin0 file and report errors in readable way
     :param kin0_file: .kin0 file output from King
@@ -121,6 +122,7 @@ def parse_kin0_errors(kin0_file, delim, cohorts, args):
         error_report.write(f"#Input kinship file: {kin0_file}\n")
         error_report.write(f"#Date and time: {time.asctime()}\n")
 
+        delim = utils.check_delimiter(kin0_file)
         with open(kin0_file) as in_kin0:
             for line in in_kin0:
                 fid1, id1, fid2, id2, n_snp, hethet, ibs0, kinship = line.strip().split(delim)
@@ -136,7 +138,7 @@ def parse_kin0_errors(kin0_file, delim, cohorts, args):
                 error_report.write(line)
 
 
-def count_families_kin(kin_file, delim, cohorts, args):
+def count_families_kin(kin_file, cohorts, kinship_cutoff):
     # Create edgelist from kin file without errors and report # of families, and size of families
     if "all" not in cohorts:
         cohorts.append("all")
@@ -149,22 +151,66 @@ def count_families_kin(kin_file, delim, cohorts, args):
         edgelist_name = f"{filestem}_{cohort}.edgelist_kin"
         edgelist = open(edgelist_name, "w")
 
+        delim = utils.check_delimiter(kin_file)
         with open(kin_file) as kin_in:
             for line in kin_in:
                 if not ((cohort == "all") | (cohort in line)):
                     continue
 
                 fid, id1, id2, n_snp, z0, phi, hethet, ibs0, kinship, error = line.strip().split(delim)
-                if fid != "FID":
-                    if error == "0":
-                        edgelist.write("\t".join([id1, id2]) + "\n")
+                if (fid != "FID") and (fid != "0") and (error == "0"):
+                    edgelist.write("\t".join([id1, id2]) + "\n")
+
+        kin_in.close()
 
         ###################################################################
         # Read in the edgelist and count the number of independent graphs #
         ###################################################################
         g = nx.read_edgelist(edgelist_name)
 
+        num_families = nx.number_connected_components(g)
 
+        families_count = {}
+        for c in nx.connected_components(g):
+            node_count = len(c)
+            if node_count not in families_count.keys():
+                families_count[node_count] = 1
+            else:
+                families_count[node_count] += 1
+            # If the node count is too high, write names to file
+            if node_count > 50:
+                print("Problematic family detected- finding individuals connected to too many others and writing "
+                      "names to file.")
+                problem_graph = g.subgraph(c)
+                most_connected = max(dict(problem_graph.degre()).items(), key=lambda x: x[1])
+
+                filename = f"family_{node_count}_members"
+                while os.path.isfile(filename):
+                    filename = filename + "-1"
+                filename = filename + ".txt"
+                out_f = open(filename, "w")
+                out_f.write("ID\tnum_connections\n")
+
+                connections = dict(problem_graph.degree())
+                connections_ordered = OrderedDict(sorted(connections.items(), key=lambda kv: kv[1], reverse=True))
+
+                for id, num_connections in connections_ordered.items():
+                    out_f.write(f"{id}\t{num_connections}\n")
+
+        families_count = OrderedDict(sorted(families_count.items()))
+        for family_size, count in families_count.items():
+            print(f"{count} families with {family_size} individuals.")
+
+
+def count_trios(kin_file, fam_file,args):
+    pass
+    ############################################################################
+    # Read in kin + fam file and count the number of complete, validated trios #
+    ############################################################################
+
+    ####################################################
+    # Read in fam file and collect dictionary of trios #
+    ####################################################
 
 
 
