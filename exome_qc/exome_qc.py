53efb5c4fe8978e71bc60ff1aa9267e558f3fce6
Lea <lea.urpa@gmail.com>
2021-08-25 11:58:43 +0300

    fixed sex-aware sample annotations using genotype but not variant filtered dataset

--- exome_qc/exome_qc.py
+++ exome_qc/exome_qc.py
@@ -316,16 +316,19 @@ if __name__ == "__main__":
         mt = mt.annotate_globals(
             sex_imputation_thresholds={'female_threshold': args.female_threshold,'male_threshold': args.male_threshold})
 
-        # Annotate sex-aware variant annotations (gt filt only to have for all
+        # Annotate sex-aware variant annotations (gt filt only to have for all variants)
         gt_filt_fn = sex_imputed.rstrip("/").replace(".mt", "") + "_GT_filtered.mt/"
         mt_gt_filt = hl.read_matrix_table(gt_filt_fn)
         mt_gt_filt = mt_gt_filt.annotate_cols(is_female_imputed=imputed_sex[mt_gt_filt.s].is_female)
+        mt_gt_filt, annotations_to_transfer = va.sex_aware_variant_annotations(mt_gt_filt, pheno_col=args.pheno_col)
 
-        mt_filtered, annotations_to_transfer = va.sex_aware_variant_annotations(mt_gt_filt, pheno_col=args.pheno_col)
+        # Annotate sex-aware sample annotations, checkpoint
         mt_filtered = sa.sex_aware_sample_annotations(mt_filtered)
-
+        for annotation in annotations_to_transfer:
+            mt_filtered = mt_filtered.annotate_rows(**{annotation: mt_gt_filt.rows()[mt_filtered.row_key][annotation]})
         mt_filtered = mt_filtered.checkpoint(filtered_annot, overwrite=True)
 
+        # Annotate main MT with variant and sample sex-aware annotations
         for annotation in annotations_to_transfer:
             mt = mt.annotate_rows(**{annotation: mt_filtered.rows()[mt.row_key][annotation]})
         mt = mt.annotate_cols(sexaware_sample_call_rate=mt_filtered.cols()[mt.s].sexaware_sample_call_rate)
